9755ad07057fab94e6a927779c7f90bcff608475
Cenk Merk <cenkmerk@gmail.com>
2023-08-15 18:54:31 +0300

    Added products data

---
+++ src/constant/index.ts
@@ -0,0 +1,44 @@
+import P1 from "./img/p1.webp";
+import P2 from "./img/p2.webp";
+import P3 from "./img/p3.webp";
+import P4 from "./img/p4.webp";
+import P5 from "./img/p5.webp";
+import P6 from "./img/p6.webp";
+import P7 from "./img/p7.webp";
+import P8 from "./img/p8.webp";
+const ProductItems = [
+  {
+    id: "1",
+    title: "Kolsuz Midi Elbise",
+    price: "299",
+    img1: P1,
+    img2: P2,
+    quantity: 1,
+  },
+  {
+    id: "2",
+    title: "Askılı Midi Elbise",
+    price: "299",
+    img1: P3,
+    img2: P4,
+    quantity: 1,
+  },
+  {
+    id: "3",
+    title: "Drapeli Elbise",
+    price: "559",
+    img1: P5,
+    img2: P6,
+    quantity: 1,
+  },
+  {
+    id: "4",
+    title: "Askılı Elbise",
+    price: "759",
+    img1: P7,
+    img2: P8,
+    quantity: 1,
+  },
+];
+
+export default ProductItems;
