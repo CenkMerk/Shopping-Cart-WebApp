61295628707a68bc403506aac7d5ddff2c4c7539
Cenk Merk <cenkmerk@gmail.com>
2023-08-15 23:08:50 +0300

    props defined

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,9 +1,27 @@
-import Stack from '@mui/material/Stack/Stack'
+import Stack from "@mui/material/Stack/Stack";
+import Typography from "@mui/material/Typography";
+import { ProductItemProps } from "../types";
 
-const ProductItem = () => {
+const ProductItem = ({
+  id,
+  img1,
+  img2,
+  price,
+  quantity,
+  title,
+}: ProductItemProps) => {
+  console.log(id, img1, img2, price, quantity, title);
   return (
-    <Stack>ProductItem</Stack>
-  )
-}
+    <Stack>
+      <img src={`${img1}`} alt={`${title}`} width="100%" />
+      <Typography variant="h5" gutterBottom>
+        h5. Heading
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        h6. Heading
+      </Typography>
+    </Stack>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
