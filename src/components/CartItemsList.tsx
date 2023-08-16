c38b988057a2760ff0323d3b6d63468a459e2506
Cenk Merk <cenkmerk@gmail.com>
2023-08-16 22:30:01 +0300

    Created CartItemList

---
+++ src/components/CartItemsList.tsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../Redux/store";
+import { Stack } from "@mui/material";
+import CartProduct from "./CartProduct";
+
+const CartItemsList = () => {
+  const selectItem = useSelector(
+    (state: RootState) => state.counter.selectItems
+  );
+
+  return (
+    <Stack>
+      {selectItem.map((item) => (
+        <CartProduct
+          id={item.id}
+          img1={item.img1}
+          price={item.price}
+          quantity={item.quantity}
+          title={item.title}
+          key={item.id}
+        />
+      ))}
+    </Stack>
+  );
+};
+
+export default CartItemsList;
