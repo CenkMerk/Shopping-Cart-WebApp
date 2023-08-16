2bf05646fd58b56bea8932dacfec4a7d43162185
Cenk Merk <cenkmerk@gmail.com>
2023-08-16 22:31:15 +0300

    Created CartProduct

---
+++ src/components/CartProduct.tsx
@@ -0,0 +1,58 @@
+import { CartItem } from "../types";
+import { Stack, Typography, Box, IconButton } from "@mui/material";
+import { AiOutlinePlus, AiOutlineMinus, AiOutlineDelete } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { increase, descrease,removeItem } from "../Redux/cartSlice";
+
+const CartProduct = ({ id, img1, price, quantity, title }: CartItem) => {
+  const dispatch = useDispatch();
+  return (
+    <Box
+      boxShadow={3}
+      margin={2}
+      flexDirection="row"
+      display="flex"
+      borderRadius={2}
+      alignItems="center"
+    >
+      <img src={`${img1}`} alt="product" width="80px" />
+      <Stack>
+        <Typography variant="subtitle1" paddingX="10px" fontWeight={600}>
+          {title}
+        </Typography>
+        <Typography variant="h6" gutterBottom paddingX="10px">
+          ${price}
+        </Typography>
+      </Stack>
+      <Stack alignItems="center" marginLeft="auto" marginRight="20px">
+        <IconButton
+          onClick={() => {
+            dispatch(increase(id));
+          }}
+        >
+          <AiOutlinePlus />
+        </IconButton>
+        <Typography>{quantity}</Typography>
+        {quantity === 1 ? (
+          <IconButton
+            onClick={() => {
+              dispatch(removeItem(id));
+            }}
+          >
+            <AiOutlineDelete />
+          </IconButton>
+        ) : (
+          <IconButton
+            onClick={() => {
+              dispatch(descrease(id));
+            }}
+          >
+            <AiOutlineMinus />
+          </IconButton>
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default CartProduct;
